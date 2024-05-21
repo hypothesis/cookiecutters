d15d28b8ec5fbd59a5a831e7b442379b70ae0398
Marcos Prieto <marcospri@gmail.com>
2024-05-21 15:18:40 +0200

    Reload templates on changes, recursively in the templates folder

--- pyramid-app/{{ cookiecutter.slug }}/conf/gunicorn-dev.conf.py
+++ pyramid-app/{{ cookiecutter.slug }}/conf/gunicorn-dev.conf.py
@@ -1,4 +1,6 @@
+from glob import glob
+
 bind = "0.0.0.0:{{ cookiecutter.port }}"
 reload = True
-reload_extra_files = "{{ cookiecutter.package_name }}/templates"
+reload_extra_files = glob("{{ cookiecutter.package_name }}/templates/**/*", recursive=True)
 timeout = 0
