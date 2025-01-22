f4c68c296d119c10bf3fdd81b7d8de84c46ce0ef
Sean Hammond <seanh@users.noreply.github.com>
2025-01-22 12:59:16 +0000

    Add .cookiecutter/includes/conf/gunicorn-dev.conf.py/tail

--- pyramid-app/{{ cookiecutter.slug }}/conf/gunicorn-dev.conf.py
+++ pyramid-app/{{ cookiecutter.slug }}/conf/gunicorn-dev.conf.py
@@ -4,3 +4,6 @@ bind = "0.0.0.0:{{ cookiecutter.port }}"
 reload = True
 reload_extra_files = glob("{{ cookiecutter.package_name }}/templates/**/*", recursive=True)
 timeout = 0
+{% if include_exists("conf/gunicorn-dev.conf.py/tail") %}
+{{ include("conf/gunicorn-dev.conf.py/tail") -}}
+{% endif %}
