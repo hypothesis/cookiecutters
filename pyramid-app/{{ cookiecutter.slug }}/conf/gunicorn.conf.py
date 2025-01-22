8ccd9592282829e150b0d1ae81c8ea56690feb23
Sean Hammond <seanh@users.noreply.github.com>
2025-01-22 13:47:51 +0000

    Add .cookiecutter/includes/conf/gunicorn.conf.py/tail

--- pyramid-app/{{ cookiecutter.slug }}/conf/gunicorn.conf.py
+++ pyramid-app/{{ cookiecutter.slug }}/conf/gunicorn.conf.py
@@ -1,2 +1,5 @@
 bind = "{{ cookiecutter.get("__gunicorn_bind") or "0.0.0.0:" + cookiecutter.port }}"
 worker_tmp_dir = "/dev/shm"
+{% if include_exists("conf/gunicorn.conf.py/tail") %}
+{{ include("conf/gunicorn.conf.py/tail") -}}
+{% endif %}
