d8b173425202abf40a9613e3655f97b5efcea0a4
Sean Hammond <seanh@users.noreply.github.com>
2025-01-28 14:23:37 +0000

    Enable many more Ruff linter rules
    ...and switch from an opt-in to an opt-out strategy where we enable ALL
    rules and then disable certain specific rules or sets of rules. This
    means that when new versions of Ruff come out new rules will be enabled
    by default.
    
    Slack discussion: https://hypothes-is.slack.com/archives/C1MA4E9B9/p1737635626518379

--- pyapp/{{ cookiecutter.slug }}/{{ cookiecutter.package_name }}/app.py
+++ pyapp/{{ cookiecutter.slug }}/{{ cookiecutter.package_name }}/app.py
@@ -5,5 +5,5 @@ from {{ cookiecutter.package_name }}.core import hello_world
 
 def run():
     while True:
-        print(hello_world())
+        print(hello_world())  # noqa: T201
         time.sleep(1)
--- pypackage/{{ cookiecutter.slug }}/tests/functional/cli_test.py
+++ pypackage/{{ cookiecutter.slug }}/tests/functional/cli_test.py
@@ -3,9 +3,9 @@ from subprocess import run
 
 def test_help():
     """Test the {{ cookiecutter.__entry_point }} --help command."""
-    run(["{{ cookiecutter.__entry_point }}", "--help"], check=True)
+    run(["{{ cookiecutter.__entry_point }}", "--help"], check=True)  # noqa: S603,S607
 
 
 def test_version():
     """Test the {{ cookiecutter.__entry_point }} --version command."""
-    run(["{{ cookiecutter.__entry_point }}", "--version"], check=True)
+    run(["{{ cookiecutter.__entry_point }}", "--version"], check=True)  # noqa: S603,S607
